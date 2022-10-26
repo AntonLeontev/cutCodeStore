665be314bd922d57f1137337f00fcf33f2de4ddc
Anton <aner-anton@ya.ru>
2022-10-26 22:47:19 +0300

    Factory for products images

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -19,7 +19,12 @@ class ProductFactory extends Factory
     {
         return [
             'title' => ucfirst($this->faker->words(2, true)),
-            'thumbnail' => '',
+            //TODO Вынести метод в отдельный провайдер. Создавать директории, если их нет. В базе сохранять путь от /storage
+            'thumbnail' => fake()->file(
+                base_path('/tests/Fixtures/images/products'),
+                storage_path('/app/public/images/products'),
+                false
+            ),
             'price' => $this->faker->numberBetween(1000, 100000),
             'brand_id' => Brand::query()->inRandomOrder()->value('id'),
         ];
